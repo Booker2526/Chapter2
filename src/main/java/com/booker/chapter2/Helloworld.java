9d9e3d0d67d7a7e904fb787c839d9546b918864c
Booker2526 <cut_20@hotmail.com>
2014-12-21 10:59:25 +0700

    firstcommit

---
+++ src/main/java/com/booker/chapter2/Helloworld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.booker.chapter2;
+
+/**
+ *
+ * @author BoY
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hellowolrd");
+    }
+    
+}
